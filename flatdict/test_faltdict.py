9cc4c8c03816baba8c2351da3c8d0e0db0e75c5f
Grumpy <grumpy@123koz.com>
2019-02-10 14:22:02 -0800

    Test FlatDict caching

--- flatdict/test_faltdict.py
+++ flatdict/test_faltdict.py
@@ -1,10 +1,23 @@
 from flatdict.flatdict import FlatDict
+from random import randint
+
+
+def repeat(s):
+    return "{s}+{s}".format(s=s)
+
+
+def get_rand(s):
+    return randint(1, 1000000000)
 
 
 def test_basic_flat_dict():
-    def repeat(s):
-        return "{s}+{s}".format(s=s)
-
     fd = FlatDict(repeat)
     assert fd.ala == "ala+ala"
-    assert 'ala' in fd.__dict__
+
+
+def test_caching():
+    fd = FlatDict(get_rand)
+    ala = fd.ala
+    assert fd.ala == ala
+    assert fd.ala != fd.beta
+
